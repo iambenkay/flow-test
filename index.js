d6ada68c8a54b0319dce7766fb1a559ef75b27fb
benkay <benjamin.node@gmail.com>
2019-09-29 07:06:20 +0100

    Fixed Heroku Postgres

--- index.js
+++ index.js
@@ -34,11 +34,10 @@ app.use((req, res, next) => {
     res.set("access-control-allow-origin", "*")
     res.set("access-control-allow-headers", "*")
     res.set("access-control-allow-methods", "*")
-
     next()
 })
 
-// cursor.sync({force: true})
+cursor.sync()
 
 app.use("/api", API_ROUTES)
 
