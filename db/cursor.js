3ea898a1518557b399e7cc2903f2714665db1075
benkay <benjamin.node@gmail.com>
2019-09-29 06:58:12 +0100

    Fixed Heroku Postgres

--- db/cursor.js
+++ db/cursor.js
@@ -7,7 +7,10 @@ const cursor = (!env)
         host: "localhost",
         dialect: "postgres",
     })
-    : new Sequelize(process.env.DATABASE_URL)
+    : new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+    })
 
 cursor.authenticate()
     .then(() => {
